969d3bcdb5ed342da7a71b65a9d37419d144d5e0
Mortier Martin <martin.mortier@gmail.com>
2018-10-18 18:38:01 +0200

    Update navbar and components + routes
    18:37

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ShowTableComponent} from './show-table/show-table.component';
+import {RouterModule, Routes} from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { FourOFourComponent } from './four-o-four/four-o-four.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'IndexComponent', component: IndexComponent
+  },
+  {
+    path: 'ShowTableComponent', component: ShowTableComponent
+  },
+  {
+    path: '', component: IndexComponent
+  },
+  {
+    path: 'FouroFour', component: FourOFourComponent
+  },
+  {
+    path: '**', redirectTo: '/FouroFour' // Obligatoirement à la fin!
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     ShowTableComponent,
+    IndexComponent,
+    FourOFourComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
