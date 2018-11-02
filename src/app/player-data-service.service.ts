8bb15473c01ede74edddec198a57e2fdbc334218
MA-Moustache <michael-genique@hotmail.com>
2018-11-02 13:53:10 +0100

    Implementing "Joueur" service, "joueur.ts" class

--- src/app/player-data-service.service.ts
+++ src/app/player-data-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Joueur} from './joueur';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerDataServiceService {
 
-  constructor() { }
+  public static readonly URL_API_JOUEUR: string = '/api/joueur';
+
+
+
+
+
+  constructor(public http: HttpClient) { }
+
+  public query(): Observable<Joueur[]> {
+    return this.http.get<Joueur[]>(PlayerDataServiceService.URL_API_JOUEUR);
+  }
+
+  public post(joueur: Joueur): Observable<Joueur[]> {
+    return this.http.post<Joueur[]>(PlayerDataServiceService.URL_API_JOUEUR, joueur.toJson());
+  }
 }
